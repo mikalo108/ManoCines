b998dd73f6898a926aa1a930eec9763731ef0845
mikalo108 <miguelcarnicerembid@gmail.com>
2025-05-20 09:58:55 +0200

    welcome v1

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,16 +1,12 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import GuestHeader from '../Components/GuestHeader';
 import { Link } from '@inertiajs/react';
 
-export default function GuestLayout({ children }) {
+export default function GuestLayout({ children, auth }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="bg-gray-50 text-black/50 dark:bg-zinc-800 dark:text-white/50">
+            <GuestHeader auth={auth} />
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <div className="bg-gray-50 text-black/50 dark:bg-zinc-800 dark:text-white/50">
                 {children}
             </div>
         </div>
---
+++ resources/js/components/GuestHeader.jsx
@@ -0,0 +1,59 @@
+import { Link } from '@inertiajs/react';
+
+export default function GuestHeader({ auth }) {
+    return (
+        <header className="grid items-center gap-2 py-10 grid-cols-3 grid-rows-1 max-md:grid-cols-1 max-md:grid-rows-3 max-md:justify-items-center" style={{ zIndex: 100, marginInline:'50px', width: '80%' }}>
+            <div className='flex lg:justify-space-evently flex items-center gap-7'>
+                <Link
+                    href={route('home')}
+                    className="flex items-center gap-2 max-md:justify-center"
+                    title="MañoCines"
+                >
+                    <img src="storage/general/logo.webp" alt="Logo" width='80vw' />
+                    <h2 className="lg:text-2xl flex font-bold text-black dark:text-white">
+                        MañoCines
+                    </h2>                
+                </Link>
+                
+            </div>
+            <nav className="max-md:hidden lg:grid lg:gap-2 lg:justify-items-center lg:grid-cols-3 lg:grid-rows-1">
+                {auth?.user ? (
+                    <Link
+                        href={route('home')}
+                        className="rounded-full px-3 py-2 text-black ring-1 ring-transparent transition hover:bg-gray-500 hover:bg-opacity-20 hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        PAGE
+                    </Link>
+                ) : (
+                    <>
+                    </>
+                )}
+            </nav>
+            <nav className="-mx-3 flex flex-1 justify-end md:justify-items-end">
+                {auth?.user ? (
+                    <Link
+                        href={route('dashboard')}
+                        className="rounded-full px-3 py-2 text-black ring-1 ring-transparent transition hover:bg-gray-500 hover:bg-opacity-20 hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Dashboard
+                    </Link>
+                ) : (
+                    <>
+                        <Link
+                            href={route('login')}
+                            className="rounded-full px-3 py-2 text-black ring-1 ring-transparent transition hover:bg-gray-500 hover:bg-opacity-20 hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            href={route('register')}
+                            className="rounded-full px-3 py-2 text-black ring-1 ring-transparent transition hover:bg-gray-500 hover:bg-opacity-20 hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
+            </nav>
+        </header>
+    );
+}

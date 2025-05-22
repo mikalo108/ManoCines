1e325467af249ce56fee04bb5f1ffa8e88c9bd62
mikalo108 <miguelcarnicerembid@gmail.com>
2025-05-23 00:15:52 +0200

    Rutinas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,35 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Models\Film;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @param Schedule $schedule
+     * @return void
      */
-    public function boot(): void
+    public function boot(Schedule $schedule)
     {
-        Vite::prefetch(concurrency: 3);
+        $schedule->command('temporalreserves:update')->everyMinute();
+
+        // Load all films ordered by number of associated orders descending and share with all views globally
+        $films = Film::withCount('orders')->orderBy('orders_count', 'desc')->get();
+        View::share('allFilms', $films);
     }
 }
